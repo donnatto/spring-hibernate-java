aedfae2e6e39a532815d6fe3077517be32f0c088
Edwin Dominguez <edwindominguez@hotmail.com>
2019-12-22 22:49:19 -0500

    add client rest controller

---
+++ src/main/java/com/donnatto/ereservation/resources/ClienteResource.java
@@ -0,0 +1,13 @@
+package com.donnatto.ereservation.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Class to represent client web service
+ * @author donnatto
+ */
+@RestController
+@RequestMapping("/api/cliente")
+public class ClienteResource {
+}
